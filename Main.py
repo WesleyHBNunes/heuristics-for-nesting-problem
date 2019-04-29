bbe54145c29bb6516d1ad021b67d700001d0d259
wesley <wesley.rn2@gmail.com>
2019-04-29 18:02:53 -0300

    Initial implementation of function better_initial_solution

--- Main.py
+++ Main.py
@@ -4,7 +4,10 @@ import BottomLeft
 
 def main():
     polygons = File.polygons_from_xls("Test/marques.xls", "Marques")
-    BottomLeft.initial_solution(polygons)
+    limits = File.return_limits_of_board_xls("Test/marques.xls", "Marques")
+    BottomLeft.initial_solution(polygons, limits[0], limits[1])
+
+    # BottomLeft.better_initial_solution(polygons, limits[0], limits[1])
 
 
 if __name__ == "__main__":
