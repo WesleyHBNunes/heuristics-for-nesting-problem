77787bc8183ff9860f17848b0b223691f2e92abf
wesley <wesley.rn2@gmail.com>
2019-04-30 18:03:18 -0300

    Shuffle on polygons list

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import File
 import BottomLeft
+from random import shuffle
 from Visualizer import Visualizer
 
 
 def main():
     polygons, limits = File.polygons_from_xls("Test/han.xls", "Han")
+    shuffle(polygons)
     visualizer = Visualizer(BottomLeft.better_initial_solution(polygons, limits[0]),
                             limits[0], limits[1], "Title animation plot")
     # visualizer.plot_polygons()
