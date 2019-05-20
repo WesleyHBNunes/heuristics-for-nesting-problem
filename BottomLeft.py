dc13b448ce049f222a2fba5e2f8ce72f806d24b7
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-05-19 22:10:19 -0300

    Function better_solution created

--- BottomLeft.py
+++ BottomLeft.py
@@ -57,7 +57,6 @@ def better_solution(array_polygons, x_lim):
         max_point_x = max(list_x)
 
         current_list_x, current_list_y = list(zip(*array_polygons[i]))
-        current_list_x = list(current_list_x)
 
         if max(current_list_x) + max_point_x < x_lim:
             point_x = max_point_x + LAMBDA
@@ -65,54 +64,21 @@ def better_solution(array_polygons, x_lim):
             point_x = 0
 
         array_polygons[i] = Polygon.add_number_axis_x_y(array_polygons[i], point_x, 0)
-        current_max_point_x = max(current_list_x)
-        current_min_point_x = min(current_list_x)
-        point_y = 0
-        if i == 4:
-            print('a')
-        for polygon in new_polygons:
-            list_x_polygon, list_y_polygon = list(zip(*polygon))
-            max_x_polygon = max(list_x_polygon)
-            min_x_polygon = min(list_x_polygon)
-            max_y_polygon = max(list_y_polygon)
-            if (current_min_point_x <= min_x_polygon <= max_x_polygon <= current_max_point_x) or (
-                    min_x_polygon <= current_min_point_x <= current_max_point_x <= max_x_polygon) or (
-                    min_x_polygon <= current_min_point_x <= max_x_polygon <= current_max_point_x) or (
-                    current_min_point_x <= min_x_polygon <= current_max_point_x <= max_x_polygon):
-                if max_x_polygon > point_y:
-                    point_y = max_y_polygon + LAMBDA
-
-        new_polygons.append(Polygon.add_number_axis_x_y(array_polygons[i], 0, point_y))
-    return Polygon.create_polygons_to_plot(new_polygons), return_line_y(new_polygons)
-
-
-def better_solution2(array_polygons, x_lim):
-    new_polygons = []
-    for i in range(len(array_polygons)):
-        if i == 0:
-            new_polygons.append(array_polygons[i])
-            continue
-        list_x, list_y = list(zip(*new_polygons[i - 1]))
-        max_point_x = max(list_x)
-
         current_list_x, current_list_y = list(zip(*array_polygons[i]))
         current_list_x = list(current_list_x)
+        current_max_point_x = max(current_list_x)
+        current_min_point_x = min(current_list_x)
 
-        if max(current_list_x) + max_point_x < x_lim:
-            point_x = max_point_x + LAMBDA
-        else:
-            point_x = 0
-
-        array_polygons[i] = Polygon.add_number_axis_x_y(array_polygons[i], point_x, 0)
         highest_y_point = return_line_y(new_polygons)
-        new_list_y = []
-        for number in current_list_y:
-            new_list_y.append(number + highest_y_point)
+        abstract_polygon = [(current_min_point_x, 0),
+                            (current_max_point_x, 0),
+                            (current_max_point_x, highest_y_point),
+                            (current_min_point_x, highest_y_point)]
 
         polygons_to_analyze = []
-        for polygons in new_polygons:
-            if Polygon.is_overlapping(array_polygons[i], polygons):
-                polygons_to_analyze.append(polygons)
+        for polygon in new_polygons:
+            if Polygon.is_overlapping(abstract_polygon, polygon):
+                polygons_to_analyze.append(polygon)
 
         point_y = return_line_y(polygons_to_analyze)
         array_polygons[i] = Polygon.add_number_axis_x_y(array_polygons[i], 0, point_y)
@@ -126,23 +92,8 @@ def random_solution(array_polygons, x_lim, function):
     return function(array_polygons, x_lim)
 
 
-def sorted_by_area_solution(array_polygons, x_lim, function):
-    array_polygons = Polygon.sort(array_polygons, Polygon.area_polygon)
-    return function(array_polygons, x_lim)
-
-
-def sorted_by_ray_solution(array_polygons, x_lim, function):
-    array_polygons = Polygon.sort(array_polygons, Polygon.ray_polygon)
-    return function(array_polygons, x_lim)
-
-
-def sorted_by_rectangle_of_polygon(array_polygons, x_lim, function):
-    array_polygons = Polygon.sort(array_polygons, Polygon.rectangle_polygon_area)
-    return function(array_polygons, x_lim)
-
-
-def sorted_by_area_no_used_of_polygon(array_polygons, x_lim, function):
-    array_polygons = Polygon.sort(array_polygons, Polygon.area_no_used_of_polygon)
+def solve(array_polygons, x_lim, function, sort_function):
+    array_polygons = Polygon.sort(array_polygons, sort_function)
     return function(array_polygons, x_lim)
 
 
--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import File
 import BottomLeft
+import Polygon
 from Visualizer import Visualizer
 
 
 def main():
     polygons, limit_x = File.polygons_from_txt("Test/trousers.txt")
-    polygons_to_plot, limit_y = BottomLeft.sorted_by_area_no_used_of_polygon(polygons, limit_x,
-                                                                             BottomLeft.trya)
+    polygons_to_plot, limit_y = BottomLeft.solve(polygons, limit_x,
+                                                 BottomLeft.better_solution, Polygon.area_no_used_of_polygon)
     visualizer = Visualizer(polygons_to_plot, limit_x, limit_y, "Test of instance trousers.txt")
     # visualizer.plot_polygons()
     visualizer.plot_animation()
